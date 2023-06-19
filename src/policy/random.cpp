2b0483cfe0895204e0a821234cb6e168cd90776a
Kevin Tandian <revenstar2@DESKTOP-KQ5PN7J>
2023-06-20 02:28:44 +0800

    first commit

---
+++ src/policy/random.cpp
@@ -0,0 +1,75 @@
+#include <cstdlib>
+#include <fstream>
+#include <limits.h>
+
+#include "../state/state.hpp"
+#include "./random.hpp"
+// std::ofstream fout("randomPol.txt", std::ios::app);
+
+static std::string y_axis = "654321";
+static std::string x_axis = "ABCDE";
+
+
+/**
+ * @brief Randomly get a legal action
+ * 
+ * @param state Now state
+ * @param depth You may need this for other policy
+ * @return Move 
+ */
+
+//Minimax code
+
+int Random::minimax(State *node, int depth, int maximizingPlayer, int alpha, int beta){
+
+  if(depth==0 || node->game_state==WIN||node->game_state==DRAW){
+      return node->evaluate();
+  }
+  if(maximizingPlayer){
+        int value = INT_MIN;
+        node->get_legal_actions();
+        auto actions = node->legal_actions;
+        for(auto i = actions.begin();i!=actions.end();i++){
+          auto child = node->next_state(*i);
+          value = std::max(value, minimax(child, depth-1, 0, alpha,beta));
+          alpha = std::max(alpha,value);
+          if(alpha>=beta){
+            break;
+      }
+  }
+      return value;
+  }
+  else{
+    int value = INT_MAX;
+    node->get_legal_actions();
+    auto actions = node->legal_actions;
+    for(auto i = actions.begin();i!=actions.end();i++){
+          auto child = node->next_state(*i);
+          value = std::min(value, minimax(child, depth-1, 1, alpha,beta));
+          beta = std::min(beta,value);
+          if(beta<=alpha){
+            break;
+          }
+    }
+    return value;
+  }
+}
+
+
+
+Move Random::get_move(State *state, int depth){
+  int bestValue = INT_MIN;
+  Move idealMove (Point(-1,-1),Point(-1,-1));
+  state->get_legal_actions();
+  auto actionsList = state->legal_actions;
+  for(auto i = actionsList.begin();i!=actionsList.end();i++){
+      State *temp = state->next_state(*i);
+      int value = minimax(temp, 5, state->player, INT_MIN,INT_MAX);
+      if(value>bestValue){
+        bestValue=value;
+        idealMove = *i;
+      }
+  }
+  return idealMove; 
+}
+
